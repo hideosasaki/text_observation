86edeac09dbc417ac0a0394da3f9f47e922c3cf6
RIO Sasaki <sasaki@rio.ne.jp>
2020-02-04 16:55:09 +0900

    Portraitにして認識範囲を設定した

--- TextObservation/TextObservationViewController.swift
+++ TextObservation/TextObservationViewController.swift
@@ -3,13 +3,23 @@ import AVFoundation
 import Vision
 
 class TextObservationViewController: UIViewController {
-
+    // 読み取り範囲の定義
+    private var readAreaX: CGFloat = 0
+    private let readAreaY: CGFloat = 540
+    private let readAreaWidth: CGFloat = 1080
+    private let readAreaHeight: CGFloat = 135
+    private var imageSizeRatio: CGFloat = 3.375
+    private var imageSizeWidth: CGFloat = 1080
+    private var imageSizeHeight: CGFloat = 1920
+    
     @IBOutlet weak var previewImageView: UIImageView!
 
+    private var textLabel = UILabel()
+    
     private let avCaptureSession = AVCaptureSession()
     
-    /// 認識制度を設定。 リアルタイム処理なので fastで
-    private let recognitionLevel : VNRequestTextRecognitionLevel = .accurate
+    /// 認識精度を設定。 リアルタイム処理なので fastで
+    private let recognitionLevel : VNRequestTextRecognitionLevel = .fast
     
     /// サポートしている言語リストを取得 （現在は英語のみ）
     private lazy var supportedRecognitionLanguages : [String] = {
@@ -20,7 +30,10 @@ class TextObservationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageSizeRatio = view.bounds.width / imageSizeWidth
+        readAreaX = view.bounds.width / imageSizeRatio / 2 - readAreaWidth / 2
         setupCamera()
+        addShadowView()
     }
 
     override func viewDidDisappear(_ animated: Bool) {
@@ -30,7 +43,7 @@ class TextObservationViewController: UIViewController {
 
     /// カメラのセットアップ
     private func setupCamera() {
-        avCaptureSession.sessionPreset = .photo
+        avCaptureSession.sessionPreset = .hd1920x1080
 
         let device = AVCaptureDevice.default(for: .video)
         let input = try! AVCaptureDeviceInput(device: device!)
@@ -44,49 +57,61 @@ class TextObservationViewController: UIViewController {
         avCaptureSession.addOutput(videoDataOutput)
         avCaptureSession.startRunning()
     }
+    
+    /// 薄黒い背景のUIViewを作成
+    private func addShadowView() {
+        // 薄黒いビューを作成
+        let shadowView = UIView(frame: view.bounds)
+        // マスクを作成
+        let maskLayer = CAShapeLayer()
+        // 切り抜くパスを作成
+        let path = UIBezierPath(rect: view.bounds)
+                
+        // 開始座標
+        let width = readAreaWidth * imageSizeRatio
+        let height = readAreaHeight * imageSizeRatio
+        let startX = readAreaX * imageSizeRatio
+        let startY = readAreaY * imageSizeRatio
 
-    /// コンテキストに矩形を描画
-    private func drawRect(_ rect: CGRect, text: String, context: CGContext) {
+        debugPrint(width, height, startX, startY)
+        // 切り抜く
+        path.move(to: CGPoint(x: startX, y: startY))
+        path.addLine(to: CGPoint(x: startX + width, y: startY))
+        path.addLine(to: CGPoint(x: startX + width, y: startY + height))
+        path.addLine(to: CGPoint(x: startX, y: startY + height))
+        path.close()
 
-        context.setLineWidth(4.0)
-        context.setStrokeColor(UIColor.green.cgColor)
-        context.stroke(rect)
-        context.setFillColor(UIColor.black.withAlphaComponent(0.6).cgColor)
-        context.fill(rect)
-        
-        drawText(text, rect: rect, context: context)
- 
+        maskLayer.path = path.cgPath
+        maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
+        shadowView.layer.mask = maskLayer
+        shadowView.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+
+        view.addSubview(shadowView)
+    }
+    
+    /// テキストビューの定義
+    private func showText(_ mes: String?) {
+        // テキストメッセージを配置
+        textLabel.numberOfLines = 4
+        textLabel.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        textLabel.frame = CGRect(x: 0, y: 395, width: 320, height: 100)
+        textLabel.textColor = UIColor.white
+        textLabel.text = mes
+        // 上辺揃えになるよう調整
+        var rect = textLabel.frame
+        textLabel.sizeToFit()
+        rect.size.height = textLabel.frame.height
+        textLabel.frame = rect
+        view.addSubview(textLabel)
     }
 
-    /// コンテキストにテキストを描画　 (そのまま描画すると文字が反転するので、反転させる必要あり）
-    private func drawText(_ text: String, rect: CGRect, context: CGContext) {
-        
-        context.saveGState()
-        defer {
-            context.restoreGState()
+    /// コンテキストに矩形を描画
+    private func drawRect(_ rect: CGRect, context: CGContext, index: Int) {
+        if index == 0 {
+            context.setStrokeColor(UIColor.red.cgColor)
+            context.setLineWidth(4.0)
+            context.stroke(rect)
         }
-
-        let transform = CGAffineTransform(scaleX: 1, y: 1)
-        context.concatenate(transform)
-        
-        guard let textStyle = NSMutableParagraphStyle.default.mutableCopy() as? NSMutableParagraphStyle else {
-            return
-        }
-        let font = UIFont.boldSystemFont(ofSize: 20)
-        let textFontAttributes = [
-            NSAttributedString.Key.font: font,
-            NSAttributedString.Key.foregroundColor: UIColor.white,
-            NSAttributedString.Key.paragraphStyle: textStyle
-        ]
-        
-        let astr = NSAttributedString(string: text, attributes: textFontAttributes)
-        let setter = CTFramesetterCreateWithAttributedString(astr)
-        let path = CGPath(rect: rect, transform: nil)
-        let frame = CTFramesetterCreateFrame(setter, CFRange(), path, nil)
-
-        context.textMatrix = CGAffineTransform.identity
-        CTFrameDraw(frame, context)
-
     }
     
     /// 文字認識情報の配列取得 (非同期)
@@ -101,7 +126,7 @@ class TextObservationViewController: UIViewController {
 
         request.recognitionLevel = recognitionLevel
         request.recognitionLanguages = supportedRecognitionLanguages
-        request.usesLanguageCorrection = true
+        request.usesLanguageCorrection = false
 
         let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:])
         try? handler.perform([request])
@@ -118,11 +143,7 @@ class TextObservationViewController: UIViewController {
     }
 
     /// 文字検出位置に矩形を描画した image を取得
-    private func getTextRectsImage(sampleBuffer :CMSampleBuffer, textObservations: [VNRecognizedTextObservation]) -> UIImage? {
-
-        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-            return nil
-        }
+    private func getTextRectsImage(imageBuffer :CVImageBuffer, textObservations: [VNRecognizedTextObservation]) -> UIImage? {
 
         CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
 
@@ -153,10 +174,9 @@ class TextObservationViewController: UIViewController {
 
         let imageSize = CGSize(width: width, height: height)
 
-        textObservations.forEach{
-            let rect = getUnfoldRect(normalizedRect: $0.boundingBox, targetSize: imageSize)
-            let text = $0.topCandidates(1).first?.string ?? "" // topCandidates に文字列候補配列が含まれている
-            self.drawRect(rect, text: text, context: newContext)
+        for (index, el) in textObservations.enumerated() {
+            let rect = getUnfoldRect(normalizedRect: el.boundingBox, targetSize: imageSize)
+            self.drawRect(rect, context: newContext, index: index)
         }
 
         CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
@@ -164,7 +184,7 @@ class TextObservationViewController: UIViewController {
         guard let imageRef = newContext.makeImage() else {
             return nil
         }
-        let image = UIImage(cgImage: imageRef, scale: 1.0, orientation: UIImage.Orientation.right)
+        let image = UIImage(cgImage: imageRef)
 
         return image
     }
@@ -175,15 +195,48 @@ extension TextObservationViewController : AVCaptureVideoDataOutputSampleBufferDe
 
     /// カメラからの映像取得デリゲート
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-            return
-        }
-        getTextObservations(pixelBuffer: pixelBuffer) { [weak self] textObservations in
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        connection.videoOrientation = .portrait
+        let ciImage: CIImage = CIImage(cvPixelBuffer: pixelBuffer)
+        let uiImage = UIImage(ciImage: ciImage)
+        
+        let croppedUiImage = uiImage.cropped(to: CGRect(x: readAreaX, y: readAreaY, width: readAreaWidth, height: readAreaHeight))!
+        guard let croppedCiImage = croppedUiImage.safeCiImage else { return }
+        guard let imageBuffer = convertToCVPixelBuffer(from: croppedCiImage) else { return }
+        
+        getTextObservations(pixelBuffer: imageBuffer) { [weak self] textObservations in
             guard let self = self else { return }
-            let image = self.getTextRectsImage(sampleBuffer: sampleBuffer, textObservations: textObservations)
+            guard let image = self.getTextRectsImage(imageBuffer: imageBuffer, textObservations: textObservations) else { return }
+            guard let allImage = uiImage.cropped(to: CGRect(x: 0, y: 0, width: self.imageSizeWidth, height: self.imageSizeHeight)) else { return }
+            let compUiImage = allImage.composite(image: image, x: self.readAreaX, y: self.readAreaY)
             DispatchQueue.main.async { [weak self] in
-                self?.previewImageView.image = image
+                self?.previewImageView.image = compUiImage
+                self?.showText(textObservations.first?.topCandidates(1).first?.string)
             }
         }
     }
+    
+    private func convertToCVPixelBuffer(from ciImage: CIImage) -> CVPixelBuffer? {
+        let size:CGSize = ciImage.extent.size
+        var pixelBuffer:CVPixelBuffer?
+        let options = [
+            kCVPixelBufferCGImageCompatibilityKey as String: true,
+            kCVPixelBufferCGBitmapContextCompatibilityKey as String: true,
+            kCVPixelBufferIOSurfacePropertiesKey as String: [:]
+        ] as [String : Any]
+        let status:CVReturn = CVPixelBufferCreate(kCFAllocatorDefault,
+                                                  Int(size.width),
+                                                  Int(size.height),
+                                                  kCVPixelFormatType_32BGRA,
+                                                  options as CFDictionary,
+                                                  &pixelBuffer)
+        let ciContext = CIContext()
+        if (status == kCVReturnSuccess && pixelBuffer != nil) {
+            ciContext.render(ciImage, to: pixelBuffer!)
+        }
+        return pixelBuffer
+    }
 }
+
+
---
+++ TextObservation/extentions.swift
@@ -0,0 +1,60 @@
+//
+//  extentions.swift
+//  TextObservation
+//
+//  Created by sasaki on 2020/02/04.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import Foundation
+import CoreImage
+import UIKit
+
+extension UIImage {
+    var safeCiImage: CIImage? {
+        return self.ciImage ?? CIImage(image: self)
+    }
+
+    var safeCgImage: CGImage? {
+        if let cgImge = self.cgImage {
+            return cgImge
+        }
+        if let ciImage = safeCiImage {
+            let context = CIContext(options: nil)
+            return context.createCGImage(ciImage, from: ciImage.extent)
+        }
+        return nil
+    }
+
+    /// Create UIImage by cropping current image with the specified rectangle.
+    ///
+    /// - parameter rect: The rectangle to crop
+    ///
+    /// - returns: The cropped image. Nil on error.
+    func cropped(to rect: CGRect) -> UIImage? {
+        guard let unwrappedCGImage = safeCgImage else {
+            return nil
+        }
+        guard let imgRef = unwrappedCGImage.cropping(to: rect) else {
+            return nil
+        }
+        return UIImage(cgImage: imgRef, scale: scale, orientation: imageOrientation)
+    }
+    
+    func composite(image: UIImage, x: CGFloat, y: CGFloat) -> UIImage? {
+
+        UIGraphicsBeginImageContextWithOptions(self.size, false, 0)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+
+        let rect = CGRect(x: x,
+                          y: y,
+                          width: image.size.width,
+                          height: image.size.height)
+        image.draw(in: rect)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image
+    }
+}
